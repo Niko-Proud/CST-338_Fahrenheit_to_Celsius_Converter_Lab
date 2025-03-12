c375636442c2d73c8a2bcfdd7c43fc5abef54b61
Niko-Proud <nproud98@gmail.com>
2025-03-12 11:22:23 -0700

    Added .ignore file from the autogenerator showed last class

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CST-338_Fahrenheight_to_Celsius_Converter"
+include(":app")
