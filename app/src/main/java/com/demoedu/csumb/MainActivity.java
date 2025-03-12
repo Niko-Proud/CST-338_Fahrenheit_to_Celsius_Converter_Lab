d628b809d38170fb72ca89c77caafedf1350ae8a
Niko-Proud <nproud98@gmail.com>
2025-03-12 11:54:12 -0700

    added View and Binding, and some text boxes, gotta rewatch vid instructions

--- app/src/main/java/com/demoedu/csumb/MainActivity.java
+++ app/src/main/java/com/demoedu/csumb/MainActivity.java
@@ -1,14 +1,25 @@
 package com.demoedu.csumb;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.demoedu.csumb.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
